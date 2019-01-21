78839def1af653121ac6821199ae86b768a6d2d0
Charley Wu <akunzai@gmail.com>
2019-01-21 13:43:15 +0800

    Fix TS-Lint warngings

--- src/app/shared/initial-value/initial-value.directive.ts
+++ src/app/shared/initial-value/initial-value.directive.ts
@@ -1,4 +1,5 @@
 import * as angular from 'angular';
+
 /**
  * init ngModel from default value
  * example: <input type="email" ng-model="email" ng-initial value="user@example.com">
@@ -6,14 +7,10 @@ import * as angular from 'angular';
 export class InitialValueDirective implements ng.IDirective {
   public require: string = 'ngModel';
   public restrict: string = 'A';
-  public static factory(): ng.IDirectiveFactory {
-    /*@ngInject*/
-    const factory: ng.IDirectiveFactory =
-      ($parse: ng.IParseService) => new InitialValueDirective($parse);
-    return factory;
-  }
+
   constructor(protected $parse: ng.IParseService) {
   }
+
   public link: ng.IDirectiveLinkFn = (
     scope: ng.IScope,
     instanceElement: ng.IAugmentedJQuery,
@@ -21,6 +18,13 @@ export class InitialValueDirective implements ng.IDirective {
   ) => {
     const val: any = instanceAttributes['ngInitial'] || instanceElement.val();
     this.$parse(instanceAttributes['ngModel']).assign(scope, val);
+  }
+
+  public static factory(): ng.IDirectiveFactory {
+    /* @ngInject */
+    const factory: ng.IDirectiveFactory =
+      ($parse: ng.IParseService) => new InitialValueDirective($parse);
+    return factory;
   }
 }
 
