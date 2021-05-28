4ff97ed57d0487c580a9c60c6e252886e316736f
Charley Wu <akunzai@gmail.com>
2021-05-28 13:56:10 +0800

    Prefer use jest-preset-angular directly
    - update to jest 27
    - remove @angular-builders/jest

---
+++ jest.global-mocks.ts
@@ -0,0 +1,28 @@
+Object.defineProperty(document, 'doctype', {
+  value: '<!DOCTYPE html>',
+});
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+/**
+ * ISSUE: https://github.com/angular/material2/issues/7101
+ * Workaround for JSDOM missing transform property
+ */
+Object.defineProperty(document.body.style, 'transform', {
+  value: () => {
+    return {
+      enumerable: true,
+      configurable: true,
+    };
+  },
+});
--- jest.setup.ts
+++ jest.setup.ts
@@ -1,6 +1,8 @@
+import 'jest-preset-angular/setup-jest';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import 'whatwg-fetch';
+import './jest.global-mocks';
 
 import { server } from './src/mocks/server';
 
