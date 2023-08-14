491e82aacac7c0668694d4172a41a2ab306e01ae
Charley Wu <akunzai@gmail.com>
2023-08-14 23:14:46 +0800

    The extend-expect script was removed

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,5 +1,4 @@
 import '@testing-library/jest-dom';
-import '@testing-library/jest-dom/extend-expect';
 import 'jest-preset-angular/setup-jest';
 import './jest.global-mocks';
 import { server } from './src/mocks/server';
